f9f44d87aad6714896dc2bcf34a1e4006c15b792
akkii7030 <96793108+akkii7030@users.noreply.github.com>
2025-02-16 19:49:39 +0530

    Costomize home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
 
       {/* Hero Section */}
       <div className="flex-grow flex flex-col justify-center items-center text-center px-6">
-        <h2 className="text-4xl font-bold text-blue-400">Portfolio</h2>
+        <h2 className="text-4xl font-bold text-blue-400">Akhilesh Vishwakarma</h2>
         <p className="mt-4 text-lg text-gray-300">
           Web Developer | React | Firebase | Tailwind CSS
         </p>
